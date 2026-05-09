a9476508f9a6578d13d59eae82e0b08b1bd214b4
NGO Portal Developer <developer@ngoportal.local>
2026-05-10 01:13:16 +0530

    Add dashboard navigation tab to admin panel

--- ngo_portal/custom_admin.py
+++ ngo_portal/custom_admin.py
@@ -1,8 +1,16 @@
 from django.contrib.admin import AdminSite
+from django.urls import reverse
+from django.http import HttpResponse
+from django.template.response import TemplateResponse
 
 class NGOAdminSite(AdminSite):
     site_header = "Inclusive Learning Hub Administration"
     site_title = "Inclusive Learning Hub Admin"
     index_title = "Welcome to the Inclusive Learning Hub Dashboard"
+
+    def index(self, request, extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['dashboard_url'] = reverse('dashboard:index')
+        return super().index(request, extra_context)
 
 ngo_admin_site = NGOAdminSite(name='ngo_admin')
